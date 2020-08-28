ddeaff4e7637d7deac3f35c5cf2ea9181d67f144
kyantaro-man <67139948+kyantaro-man@users.noreply.github.com>
2020-08-28 11:10:21 +0900

    Basic Trahack

---
+++ src/firebase/config.js
@@ -0,0 +1,12 @@
+const firebaseConfig = {
+  apiKey: "AIzaSyAPdw2Mv658fi2J0NQRDgSD7Rfs3XQQWnA",
+  authDomain: "chatbot-shin.firebaseapp.com",
+  databaseURL: "https://chatbot-shin.firebaseio.com",
+  projectId: "chatbot-shin",
+  storageBucket: "chatbot-shin.appspot.com",
+  messagingSenderId: "365430469786",
+  appId: "1:365430469786:web:5a3428162975d573961b23",
+  measurementId: "G-KP5BGT1T3F"
+};
+
+export default firebaseConfig
